063b4b2ecaccbcb5c83cadbc4526e6e1a38c2f45
Nicholas-Watkins-Projects <161062138+Nicholas-Watkins-Projects@users.noreply.github.com>
2026-04-08 14:36:18 -0400

    Highscore handling works now
    Edited how file checking worked.
    File should now keep top 5 scores.

--- src/Minesweeper.Console/Program.cs
+++ src/Minesweeper.Console/Program.cs
@@ -1,7 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using System.Security.Cryptography.X509Certificates;
 using System;
 namespace Minesweeper.Core;
--- src/Minesweeper.Core/CSVTest.cs
+++ src/Minesweeper.Core/CSVTest.cs
@@ -1,6 +1,8 @@
 
 using static System.Runtime.InteropServices.JavaScript.JSType;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
 public class CSVTest
 {
     private int _size;
@@ -10,52 +12,89 @@ public class CSVTest
     private string _timeStamp;
     private string saveFile = "../../save.csv";
     private List<string> saveInfo;
-    private List<string> size8;
-    private List<string> size12;
-    private List<string> size16;
-
-    public void CheckSaveFile()
+    private List<string> size8 = new List<string>();
+    private List<string> size12 = new List<string>();
+    private List<string> size16 = new List<string>();
+    private List<string> highscores = new List<string>();
+    private void CheckSaveFile() // Creates the save file if it doesn't exist
     {
         if (!File.Exists(saveFile))
         {
-            File.Create(saveFile);
-            File.WriteAllText(saveFile, "size,seconds,moves,seed,timestamp\n");
+            File.Create(saveFile).Close();
+            File.AppendAllText(saveFile, "size,seconds,moves,seed,timestamp\n");
         }
     }
-
-
-
-
-
-
-
-    public void Save()
+    private List<string> SelectionSort(List<string> data)
     {
-        CheckSaveFile();
-        _timeStamp = DateTime.Now.ToString("MM/dd/yy hh:mm tt");
-        File.AppendAllText(saveFile, $"{_size},{_seconds},{_moves},{_seed},{_timeStamp}\n");
+        for (int i = 0; i < data.Count - 1; i++)
+        {
+            int minIndex = i;
+            for (int j = i + 1; j < data.Count; j++)
+            {
+                if (float.Parse(data[j].Split(",")[1]) < float.Parse(data[minIndex].Split(",")[1]))
+                {
+                    minIndex = j;
+                }
+            }
+            (data[i], data[minIndex]) = (data[minIndex], data[i]);
+        }
+        return data;
     }
+    private void OrganizeSaves()
+    {
+        List<string> sortedSaves = SelectionSort(size8);
+        size8 = sortedSaves;
+        sortedSaves = SelectionSort(size12);
+        size12 = sortedSaves;
+        sortedSaves = SelectionSort(size16);
+        size16 = sortedSaves;
 
+        size8 = size8.Take(5).ToList();
+        size12 = size12.Take(5).ToList();
+        size16 = size16.Take(5).ToList();
 
-
-    public void Retrieve()
+        highscores.AddRange(size8);
+        highscores.AddRange(size12);
+        highscores.AddRange(size16);
+    }
+    private void Retrieve()
     {
         saveInfo = File.ReadAllLines(saveFile).ToList();
         saveInfo.RemoveAt(0); // Make sure the header is not in list
+        foreach (string item in saveInfo)
+        {
+            SortToSize(item);
+        }
     }
-
-
-
-
-
-
-
-
+    private void SortToSize(string save)
+    {
+        string[] items = save.Split(",");
+        if (items[0] == "8")
+            size8.Add(save);
+        if (items[0] == "12")
+            size12.Add(save);
+        if (items[0] == "16")
+            size16.Add(save);
+    }
     public void Update(int size, float seconds, int moves, int seed)
     {
         _size = size;
         _seconds = seconds;
         _moves = moves;
         _seed = seed;
+    }
+    public void Save() // Saves the file with highscores
+    {
+        CheckSaveFile();
+        Retrieve();
+
+        _timeStamp = DateTime.Now.ToString("MM/dd/yy hh:mm tt");
+        string newSave = $"{_size},{_seconds},{_moves},{_seed},{_timeStamp}";
+        SortToSize(newSave);
+
+        OrganizeSaves();
+        File.WriteAllText(saveFile, "size,seconds,moves,seed,timestamp\n");
+
+        File.AppendAllLines(saveFile, highscores);
     }
 }
